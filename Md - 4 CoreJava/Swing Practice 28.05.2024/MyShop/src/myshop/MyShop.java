5355b88ead17940d7e78747ee682e680dcccec1b
Tamim Ahmed Raju <138282465+mrahmed17@users.noreply.github.com>
2024-05-29 18:19:46 +0600

    Swing practice project

---
+++ Md - 4 CoreJava/Swing Practice 28.05.2024/MyShop/src/myshop/MyShop.java
@@ -0,0 +1,16 @@
+
+package myshop;
+
+import view.ProductView;
+
+public class MyShop {
+
+    public static void main(String[] args) {
+       
+        ProductView productView=new ProductView();
+        productView.setVisible(true);
+        
+        
+    }
+    
+}
